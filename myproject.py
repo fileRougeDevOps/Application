6c67706736c7077814e9267d5daf726a6f87f624
SimonRosPro <simon.rossignol53@gmail.com>
2021-12-17 09:38:56 +0100

    V2

--- myproject.py
+++ myproject.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 def hello():
 
-    return "<h1 style='color:blue'>Demo réussie !</h1>"
+    return "<h1 style='color:blue'>Demo réussie V2!</h1>"
 
 
 if __name__ == "__main__":
